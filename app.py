49343821e813d665fe09f0c2a360e0190a8dfb04
cspagnesi <73246281+cspagnesi@users.noreply.github.com>
2021-02-23 13:20:33 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from constants import PLAYERS
+from constants import TEAMS
+
+def clean_data():
+   for key, value in PLAYERS.items():
+      print (f"{key}: {value}")
+    
+    
+if __name__ == "__main__":
+    print ("This is indeed the 'MAIN'.  Continue...")
+    clean_data()
+    
+else:
+    "This is not the main directory."
+ 
+    
+HERE I AM
