10053b1a9395020a34c53e9edab3f3ee7194a205
cspagnesi <73246281+cspagnesi@users.noreply.github.com>
2021-02-24 08:41:32 -0500

    Let's see...

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
-from constants import PLAYERS
-from constants import TEAMS
+import constants
+import copy
 
-def clean_data():
-   for key, value in PLAYERS.items():
-      print (f"{key}: {value}")
-    
-    
+teams_list = copy.constants(TEAMS)
+     
 if __name__ == "__main__":
-    print ("This is indeed the 'MAIN'.  Continue...")
-    clean_data()
+        print ("This is indeed the 'MAIN'.  Continue...")
     
 else:
     "This is not the main directory."
- 
-    
-HERE I AM
